14e01fe888a4995a63c5adbe5aeeaa794474ff64
fox0 <red-fox0@yandex.ru>
2020-02-21 21:15:20 +0700

    save

--- wikibot.py
+++ wikibot.py
@@ -1,8 +1,8 @@
+import time
+import random
 import logging
 import subprocess
-
 import requests
-
 from private_settings import USERNAME, PASSWORD
 
 log = logging.getLogger(__name__)
@@ -14,19 +14,17 @@ class WikiAPI:
     }
     url = 'https://ru.wikipedia.org/w/api.php'
 
-    # https://ru.wikipedia.org/w/rest.php
-
     def __init__(self):
         self.s = requests.Session()
 
-    def login(self):
+    def login(self, username, password):
         login_token = self.get_login_token()
         # As of MediaWiki 1.27, using the main account for login is not supported.
         # Obtain credentials via Special:BotPasswords or use clientlogin method.
         r = self._post({
             'action': 'clientlogin',
-            'username': USERNAME,
-            'password': PASSWORD,
+            'username': username,
+            'password': password,
             'loginreturnurl': 'http://127.0.0.1:5000/',
             # At least one of the parameters "logincontinue" and "loginreturnurl" is required.
             'logintoken': login_token,
@@ -62,7 +60,7 @@ class WikiAPI:
     def save_page(self, title, text):
         token = self.get_token()
         r = self._post({'action': 'edit', 'title': title, 'text': text, 'token': token})
-        return r
+        return r['edit']['result'] == 'Success'
 
     def _get(self, params):
         return self._request('GET', params=params)
@@ -80,22 +78,34 @@ class WikiAPI:
 
 def run(api, title):
     if api.is_stable(title):
-        log.debug('is_stable')
-        return
+        log.warning('is_stable')
+        return False
 
     text = api.get_page(title)
     p = subprocess.run(('js', 'wikificator.js'), stdout=subprocess.PIPE, input=text, encoding='utf-8')
     text2 = p.stdout
     if text2 == text:
-        log.debug('==')
-        return
+        log.warning('==')
+        return False
 
-    print(api.save_page(title, text2))
+    return api.save_page(title, text2)
 
 
 def main():
     api = WikiAPI()
-    api.login()
+    api.login(USERNAME, PASSWORD)
+
+    # todo https://tools.wmflabs.org/checkwiki/cgi-bin/checkwiki.cgi?project=ruwiki&view=bots&id=64&offset=0
+    with open('64.txt') as f:
+        ls = f.readlines()
+        while True:
+            title = random.choice(ls).strip()
+            log.debug(title)
+            if run(api, title):
+                requests.get('https://tools.wmflabs.org/checkwiki/cgi-bin/checkwiki.cgi', {
+                    'project': 'ruwiki', 'view': 'only', 'id': 64, 'title': title})
+                break
+            # time.sleep(65)
 
 
 if __name__ == '__main__':
